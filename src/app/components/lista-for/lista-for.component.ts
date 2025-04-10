83183637582f7bd44b90e61377742cbee122a984
Vini-Shuet <vinishuet@gmail.com>
2025-04-10 11:27:16 -0300

    Exercicio 3

--- src/app/components/lista-for/lista-for.component.ts
+++ src/app/components/lista-for/lista-for.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lista-for',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lista-for.component.html',
   styleUrl: './lista-for.component.css'
 })
 export class ListaForComponent {
-
+  itens = ["Maçã", "Banana", "Leite"]
 }
