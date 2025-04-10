3cbc81af50ead21f54532a53ccee91a4f08b39ef
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-10 19:15:05 -0300

    Exercicio 11 - Final

--- src/app/components/calculadora/calculadora.component.ts
+++ src/app/components/calculadora/calculadora.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-calculadora',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './calculadora.component.html',
-  styleUrl: './calculadora.component.css'
+  styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent {
+  operacao: string = "soma";
+  valor1: number = 0;
+  valor2: number = 0;
 
+  trocarOperacao(event: Event): void {
+    const elementoSelecionado = event.target as HTMLSelectElement;
+    this.operacao = elementoSelecionado.value;
+  }
+
+  calcular(): void {
+  }
 }
