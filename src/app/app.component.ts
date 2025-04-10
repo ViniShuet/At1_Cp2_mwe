e4bf6801a5f3a1e92cd9ff69bfab8b1e0fb05343
Vini-Shuet <vinishuet@gmail.com>
2025-04-10 14:48:05 -0300

    Exercicio 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,25 @@ import { IfElseComponent } from './components/if-else/if-else.component';
 import { ListaDinamicaComponent } from './components/lista-dinamica/lista-dinamica.component';
 import { Switchex6Component } from './components/switchex6/switchex6.component';
 import { ForEx7Component } from './components/for-ex7/for-ex7.component';
+import { SwitchEx8Component } from './components/switch-ex8/switch-ex8.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SwitchComponent, ListaForComponent, IfElseComponent, ListaDinamicaComponent, Switchex6Component,ForEx7Component],
+  imports: [
+    RouterOutlet, 
+    SwitchComponent, 
+    ListaForComponent, 
+    IfElseComponent, 
+    ListaDinamicaComponent, 
+    Switchex6Component, 
+    ForEx7Component, 
+    SwitchEx8Component
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app-at1-cp2';
-  
+
 }
---
+++ src/app/components/switch-ex8/switch-ex8.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-ex8',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './switch-ex8.component.html',
+  styleUrl: './switch-ex8.component.css'
+})
+export class SwitchEx8Component {
+  
+}
