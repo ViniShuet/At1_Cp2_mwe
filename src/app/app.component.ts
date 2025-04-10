d30f2f88e8f26b4f76d6afbf36566a1aaa711abf
ViniShuet <vinishuet@gmail.com>
2025-04-09 22:27:50 -0300

    At1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwitchComponent } from './components/switch/switch.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SwitchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app-at1-cp2';
+  
 }
---
+++ src/app/components/switch/switch.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './switch.component.html',
+  styleUrl: './switch.component.css'
+})
+export class SwitchComponent {
+  mes:number = 1;
+  itemSelecionado :string = "um";
+
+  trocarValor(event:Event) : void{
+    const elementoSelecionado = event.target as HTMLSelectElement
+    this.mes = parseInt(elementoSelecionado.value, 10)
+  }
+}
