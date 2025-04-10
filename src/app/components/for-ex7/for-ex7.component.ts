b238e17f621cadead8b84a13ab5e50a2d3f4f2ba
Vini-Shuet <vinishuet@gmail.com>
2025-04-10 13:36:38 -0300

    ex7

---
+++ src/app/components/for-ex7/for-ex7.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-for-ex7',
+  standalone: true,
+  imports: [],
+  templateUrl: './for-ex7.component.html',
+  styleUrl: './for-ex7.component.css'
+})
+export class ForEx7Component {
+
+}
