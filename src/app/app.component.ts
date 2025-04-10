049dbf52b22b539541d754b6909140bd32bbde0b
Vini-Shuet <vinishuet@gmail.com>
2025-04-10 11:43:10 -0300

    Exercicio 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SwitchComponent } from './components/switch/switch.component';
 import { ListaForComponent } from './components/lista-for/lista-for.component';
+import { IfElseComponent } from './components/if-else/if-else.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SwitchComponent, ListaForComponent],
+  imports: [RouterOutlet, SwitchComponent, ListaForComponent, IfElseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/if-else/if-else.component.ts
+++ src/app/components/if-else/if-else.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-if-else',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './if-else.component.html',
   styleUrl: './if-else.component.css'
 })
 export class IfElseComponent {
+  idade:number = 1;
 
+  aumentarIdade(): void {
+    this.idade++;
+  }
+
+  diminuirIdade(): void {
+      this.idade--;
+  }
+
+  trocarIdade(event: Event): void {
+    const elementoSelecionado = event.target as HTMLInputElement;
+    this.idade = parseInt(elementoSelecionado.value, 10);
+  }
 }
