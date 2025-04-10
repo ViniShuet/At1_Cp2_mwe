4ca563f7a4a8d53cd2de2fc34eac19c3851a458d
Vini-Shuet <vinishuet@gmail.com>
2025-04-10 14:56:07 -0300

    Exercicio 8

--- src/app/components/switch-ex8/switch-ex8.component.ts
+++ src/app/components/switch-ex8/switch-ex8.component.ts
@@ -9,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrl: './switch-ex8.component.css'
 })
 export class SwitchEx8Component {
-  
+  valor:number = 1;
+  valor2:string = "dois";
+  // Crirei o valor 3 somente para validar que o valor nao sera conhecido
+  valor3:string = "tres"
 }
