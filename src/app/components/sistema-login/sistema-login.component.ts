215df110fb9e6b87a2dae77cdeb656420af9f74a
Vini-Shuet <vinishuet@gmail.com>
2025-04-10 15:51:02 -0300

    Exercicio 9

--- src/app/components/sistema-login/sistema-login.component.ts
+++ src/app/components/sistema-login/sistema-login.component.ts
@@ -1,14 +1,39 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sistema-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './sistema-login.component.html',
-  styleUrl: './sistema-login.component.css'
+  styleUrls: ['./sistema-login.component.css']
 })
 export class SistemaLoginComponent {
-  email:string = "user@teste.com"
-  senha:number = 123456
+  email: string = "";
+  senha: string = "";
+  mensagem: string = "";
+  loginSucesso: boolean = false;
+
+  onSubmit() {
+    if (!this.email || !this.senha) {
+      this.mensagem = "Preencha todos os campos";
+      this.loginSucesso = false;
+      return;
+    }
+
+    if (this.senha.length < 6) {
+      this.mensagem = "Senha deve ter pelo menos 6 caracteres";
+      this.loginSucesso = false;
+      return;
+    }
+
+    if (this.email === "user@teste.com" && this.senha === "123456") {
+      this.mensagem = "Login bem-sucedido!";
+      this.loginSucesso = true;
+    } else {
+      this.mensagem = "Acesso negado: Credenciais inválidas";
+      this.loginSucesso = false;
+    }
+  }
 }
